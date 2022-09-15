60f7e96c86019ce12d2fec254b39ce313af36a85
sutep_jantawee <bananammm0001@gmail.com>
2022-09-15 12:50:58 +0700

    Edit Homepage

--- pages/Masonic.js
+++ pages/Masonic.js
@@ -1,19 +1,19 @@
 import Footer from "./component/footer";
-import Image from "next/image";
 import Navbar from "./component/navbar";
 export default function Masonic() {
   return (
     <div className="font-serif">
       <Navbar />
-      <div className="pt-32  xl:mx-[223px] md:mx-[100px] sm:mx-[70px] fold:mx-[20px] mx-[10px] pb-16">
-        <p className="text-3xl">Masonic Visitors</p>
-       
-        <p className=" ml-6 pt-5">
-        All Masonic Visitors from Grand Lodges in amity with the Grand Lodge of Scotland are welcome.
-<br/>
-Please take note of our meetings in our Agenda and contact the Lodge Secretary at LDxxx@gmail.com
-        </p>
-       
+
+      <div className="pt-32 grid mx-[30px] xl:mx-[223px] md:mx-[120px] sm:mx-[70px] ">
+        <p className="text-3xl text-[24px] text-left ">Masonic Visitors</p>
+        <div className="text-xl pt-8 pb-96 text-[16px] ">
+          All Masonic Visitors from Grand Lodges in amity with the Grand Lodge
+          of Scotland are welcome.
+          <br />
+          Please take note of our meetings in our Agenda and contact the Lodge
+          Secretary at LDxxx@gmail.com
+        </div>
       </div>
 
       <Footer />
